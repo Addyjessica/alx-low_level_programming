e47ad722046952cf3919dc5696dad2662ab3b05f
Addy Jessica <addyjessica6@gmail.com>
2023-09-08 20:28:52 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int tens, ones;
+
+	 for (tens = 0; tens <= 8; ++tens)
+	 {
+		 for (ones = tens + 1; ones <= 9; ++ones)
+		 {
+			 putchar('0' + tens);
+			 putchar('0' + ones);
+			  if (tens != 8 || ones != 9)
+			  {
+				  putchar(',');
+				  putchar(' ');
+			  }
+		 }
+	 }
+	  putchar('\n');
+	  return (0);
+}
