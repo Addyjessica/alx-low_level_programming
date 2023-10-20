4d603b5d158263cccfe5d1b74ffa91c6fd37b3a7
Addy Jessica <addyjessica6@gmail.com>
2023-10-19 19:38:45 -0700

    0x12

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * list_len - returns the number of memebers in a linked list
  * @h: pointer to the list_t list
  *
- * Return: n
+ * Return: a
  */
 size_t list_len(const list_t *h)
 {
--- 0x12-singly_linked_lists/101-hello_holberson.asm
+++ 0x12-singly_linked_lists/101-hello_holberson.asm
@@ -1,11 +1,11 @@
 global   main
 	  extern    printf
 main:
-	  mov   edi, formatting
+	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
 	  mov   eax, 0
 	  ret
-formatting: db `Hello, Holberton\n`,0
+format: db `Hello, Holberton\n`,0
 
 
