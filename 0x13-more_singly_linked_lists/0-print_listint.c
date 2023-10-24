b921471ed0f291fd670d78ceeccfe379a9ba52dd
Addy Jessica <addyjessica6@gmail.com>
2023-10-24 16:15:20 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,22 +2,22 @@
 #include "lists.h"
 
 /**
- * print_listint - prints out all the elements of a linked list
+ * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: the number of nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		number++;
+		count++;
 		h = h->next;
 	}
 
-	return (number);
+	return (count);
 }
 
