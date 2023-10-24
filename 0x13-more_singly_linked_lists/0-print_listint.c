1289a2a2b0948a4e3acb5a17261a6604802f6c15
Addy Jessica <addyjessica6@gmail.com>
2023-10-24 07:53:34 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * print_listint - prints out all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: num_ of nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
