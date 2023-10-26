920c020339e8fe07fab790bf9992f6c220dc63e9
Addy Jessica <addyjessica6@gmail.com>
2023-10-26 15:17:02 -0700

    0x14

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,12 +13,14 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int x = 63;
 	int count = 0;
 	unsigned long int current;
-	unsigned long int power = n ^ m;
+	unsigned long int raised_power = n ^ m;
 
 	for (; x >= 0; x--)
 	{
-		current = power >> x;
+		current = raised_power >> x;
 		if (current & 1)
 			count++;
 	}
+	return (count);
+}
 
